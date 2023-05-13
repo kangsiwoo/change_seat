93c6ca5f797f4d6aa9731e46f7949af479d8d88a
kangsiwoo <2023003@bssm.hs.kr>
2023-05-14 06:39:41 +0900

    reset seat

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,28 @@
-fun main(args: Array<String>) {
+import java.util.*
 
+var seat: Array<Int> = Array(100, {0})
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    var studentSeatNumber: Array<Int>
+    println("학생들의 수를 입력해 주세요.")
+    var studentNumber: Int = scanner.nextInt()
+    println("자리 배치를 입력해 주세요.(입력양식 : 가로 세로)")
+    var studentI: Int = scanner.nextInt()
+    var studentJ: Int = scanner.nextInt()
+}
+
+
+public class student
+{
+    companion object
+    {
+        fun reset(num: Int) {
+            for (num in 0 .. num-1)
+            {
+                seat.set(num, 0)
+            }
+        }
+
+    }
 }
